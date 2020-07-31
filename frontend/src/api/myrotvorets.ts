48e692e9b8ad6ad52ccdf6e68929a490b570d99f
Myrotvorets <support@myrotvorets.center>
2020-07-31 23:22:32 +0300

    Deduplicate code

--- frontend/src/api/myrotvorets.ts
+++ frontend/src/api/myrotvorets.ts
@@ -25,48 +25,38 @@ interface ErrorResponse {
 
 const map = new Map<number, Criminal>();
 
-export function findCriminalBySlug(slug: string): Promise<Criminal | Error> {
-    return new Promise((resolve: (r: Criminal | Error) => void) => {
-        fetch(`https://api.myrotvorets.center/simplesearch/v1/byslug/${encodeURIComponent(slug)}`)
-            .then((r: Response): Promise<Criminal | ErrorResponse> => r.json())
-            .then((r: Criminal | ErrorResponse): void => {
+function apiCall(url: string): Promise<Criminal | Error> {
+    return fetch(`${url}}`)
+        .then((r: Response): Promise<Criminal | ErrorResponse> => r.json())
+        .then(
+            (r: Criminal | ErrorResponse): Promise<Criminal | Error> => {
                 if ('id' in r) {
                     map.set(r.id, r);
-                    resolve(r);
-                } else if (r.statusCode === 404) {
-                    resolve(new Error('NOT_FOUND'));
-                } else if ('message' in r) {
+                    return Promise.resolve(r);
+                }
+
+                if (r.statusCode === 404) {
+                    return Promise.resolve(new Error('NOT_FOUND'));
+                }
+
+                if ('message' in r) {
                     const e = new Error('API_ERROR');
                     e.message = r.message;
-                    resolve(e);
-                } else {
-                    resolve(new Error('UNKNOWN_ERROR'));
+                    return Promise.resolve(e);
                 }
-            })
-            .catch(resolve);
-    });
+
+                return Promise.resolve(new Error('UNKNOWN_ERROR'));
+            },
+        )
+        .catch((e: Error) => Promise.resolve(e));
+}
+
+export function findCriminalBySlug(slug: string): Promise<Criminal | Error> {
+    return apiCall(`https://api.myrotvorets.center/simplesearch/v1/byslug/${encodeURIComponent(slug)}`);
 }
 
 export function findCriminalById(id: number): Promise<Criminal | Error> {
-    return new Promise((resolve: (r: Criminal | Error) => void) => {
-        fetch(`https://api.myrotvorets.center/simplesearch/v1/${id}`)
-            .then((r: Response): Promise<Criminal | ErrorResponse> => r.json())
-            .then((r: Criminal | ErrorResponse): void => {
-                if ('id' in r) {
-                    map.set(r.id, r);
-                    resolve(r);
-                } else if (r.statusCode === 404) {
-                    resolve(new Error('NOT_FOUND'));
-                } else if ('message' in r) {
-                    const e = new Error('API_ERROR');
-                    e.message = r.message;
-                    resolve(e);
-                } else {
-                    resolve(new Error('UNKNOWN_ERROR'));
-                }
-            })
-            .catch(resolve);
-    });
+    return apiCall(`https://api.myrotvorets.center/simplesearch/v1/${id}`);
 }
 
 export function isKnownCriminal(id: number): Criminal | false {
