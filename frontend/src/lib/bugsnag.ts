3f8ec5b9b291b7a8d195ef95e5b2529c7605addb
Myrotvorets <support@myrotvorets.center>
2021-02-28 23:28:49 +0200

    Refactoring

--- frontend/src/lib/bugsnag.ts
+++ frontend/src/lib/bugsnag.ts
@@ -9,19 +9,21 @@ export function startBugsnag(): void {
         appType: 'web',
         releaseStage: process.env.NODE_ENV || 'development',
         onError: (e) => {
-            const connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
             e.addMetadata('webdriver', {
                 webdriver: navigator.webdriver,
             });
 
-            e.addMetadata('connection', {
-                type: connection?.type || '',
-                effectiveType: connection?.effectiveType || '',
-                downlink: connection?.downlink ?? -1,
-                downlinkMax: connection?.downlinkMax ?? -1,
-                rtt: connection?.rtt ?? -1,
-                saveData: connection?.saveData ?? false,
-            });
+            const connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+            if (connection) {
+                e.addMetadata('connection', {
+                    type: connection.type || '',
+                    effectiveType: connection.effectiveType || '',
+                    downlink: connection.downlink ?? -1,
+                    downlinkMax: connection.downlinkMax ?? -1,
+                    rtt: connection.rtt ?? -1,
+                    saveData: connection.saveData ?? false,
+                });
+            }
         },
     });
 }
